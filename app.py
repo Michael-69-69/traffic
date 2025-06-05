2394aa6b7475f461e329fa40c371f92addd5a40d
Juuzo26 <114993656+Juuzo26@users.noreply.github.com>
2025-06-05 19:29:33 +0700

    Update app.py change port?

--- app.py
+++ app.py
@@ -419,5 +419,5 @@ if __name__ == "__main__":
     logger.info("Density worker thread started")
 
     # Run Flask app on Render's PORT
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port, debug=False)
